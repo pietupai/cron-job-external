c8a7befb421cdd8e1e2f329ec92bcc16a53bad69
pietupai <pietupai@gmail.com>
2025-03-01 14:10:37 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,22 +1,15 @@
-const http = require('http');
+const fetch = require('node-fetch');
 
-function makeRequest() {
-  http.get('https://webhook-x19y.onrender.com/api/poll', (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    resp.on('end', () => {
-      console.log(data);
-    });
-
-  }).on('error', (err) => {
-    console.log('Error: ' + err.message);
-  });
+async function makeRequest() {
+  try {
+    const response = await fetch('https://webhook-x19y.onrender.com/api/poll');
+    const data = await response.text();
+    console.log(data);
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
 }
 
-setInterval(makeRequest, 180000); // 10000 milliseconds = 10 seconds
+setInterval(makeRequest, 180000); // 180000 milliseconds = 180 seconds
 
 module.exports = makeRequest;
